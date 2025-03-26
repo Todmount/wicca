c0b37e4755c23f6f693d74e9b918edaaa17bef41
Todmount <todmount@gmail.com>
2025-03-26 14:11:22 +0200

    update test

--- tests/test.py
+++ tests/test.py
@@ -11,8 +11,6 @@ from utility.wavelet_coder import HaarCoder
 from utility.classifying_tools import load_models, ClassifierProcessor
 from settings.constants import *
 
-import tensorflow
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # 0=DEBUG, 1=INFO, 2=WARNING, 3=ERROR
 
 if __name__ == '__main__':
@@ -57,24 +55,40 @@ if __name__ == '__main__':
         'DenseNet121': apps.densenet.DenseNet121,
     }
 
-    # classifiers = load_models(models_to_load) # around 1 minute
-    classifiers = load_models(models4test)
+    standard_shape_models_dict = {
+        'MobileNetV2': apps.mobilenet_v2.MobileNetV2,
+        'VGG16': apps.vgg16.VGG16,
+        'VGG19': apps.vgg19.VGG19,
+        'ResNet50': apps.resnet50.ResNet50,
+        'ResNet101': apps.resnet.ResNet101,
+        'DenseNet169': apps.densenet.DenseNet169,
+        'DenseNet201': apps.densenet.DenseNet201,
+        # 'NASNetMobile': apps.nasnet.NASNetMobile,
+        # 'ResNet152': apps.resnet.ResNet152,
+        # 'EfficientNetB0': apps.efficientnet.EfficientNetB0,
+        # 'DenseNet121': apps.densenet.DenseNet121,
+        # 'EfficientNetB1': apps.efficientnet.EfficientNetB1,
+    }
 
-    # path =  PROJECT_ROOT / 'data' / 'orig'
-    path = PROJECT_ROOT / 'data' / '4test'  # for test purposes
-    res_folder = PROJECT_ROOT / 'results'
+    # classifiers = load_models(models_to_load) # around 1 minute
+    classifiers = load_models(standard_shape_models_dict)
+
+    # data_folder =  PROJECT_ROOT / 'data' / 'orig'
+    data_folder = PROJECT_ROOT / 'data' / '4test'  # for test purposes
+    res_folder = PROJECT_ROOT / 'results' / 'test'
+    res_folder_nonexist = f'{res_folder}_nonexist'
 
     depths = range(3, 6)
-    processor = ClassifierProcessor(path=path,
-                                    coder=HaarCoder(),  # defines our wavelet
-                                    depth=depths,  # defines the depth of transforming
-                                    top=5,  # defines top classes for comparison
+    processor = ClassifierProcessor(data_folder=data_folder,
+                                    wavelet_coder=HaarCoder(),  # defines our wavelet
+                                    transform_depth=5,  # defines the depth of transforming
+                                    top_classes=5,  # defines top classes for comparison
                                     interpolation=cv2.INTER_AREA,  # ATTENTION
                                     results_folder=res_folder,
-                                    rsltmgr=rsltmgr)
+                                    result_manager=rsltmgr)
 
     ## must work for all classifiers
-    # result_all_depths = processor.process_classifiers(classifiers, timeout=100) # results for multiple all
+    result_all_depths = processor.process_classifiers(classifiers, timeout=100) # results for multiple all
 
     ## must work for single
     # result_VGG19 = processor.process_single_classifier("VGG19", classifiers["VGG19"], timeout=3601)
@@ -92,24 +106,23 @@ if __name__ == '__main__':
     # result_VGG19 = processor.process_single_classifier(classifiers["VGG19"])
 
     """VISUALIZATION"""
-    # load image
-    list_dir = [f.name for f in path.iterdir()]
-    print(f'Number of images: {len(list_dir)} \n')
-    name = list_dir[0]
-    sample = load_image(path / name)
+    ## load image
+    list_dir = [f.name for f in data_folder.iterdir()]
+    idx = list_dir[2]
+    sample = load_image(data_folder / idx)
 
     # viz.show_image_vs_icon(sample,depths, coder=HaarCoder())
-
-    # viz.dwt_visualization(sample, depths, border_width=2, border_color=(0, 0, 255), coder=HaarCoder())
+    #
+    # viz.show_icon_on_image(sample, depths, border_width=2, border_color=(0, 0, 255), coder=HaarCoder())
 
     # x = rsltmgr.compare_summaries(res_folder, classifiers, depths)
 
-    # comparison = rsltmgr.compare_summaries(res_folder, classifiers, 5, 'mean')
-    # names, similar_classes_pct = rsltmgr.extract_from_comparison(comparison, 'similar classes (%)')
-    # names, similar_best_class = rsltmgr.extract_from_comparison(comparison, 'similar best class')
+    comparison = rsltmgr.compare_summaries(res_folder, classifiers, 5, 'mean')
+    names, similar_classes_pct = rsltmgr.extract_from_comparison(comparison, 'similar classes (%)')
+    names, similar_best_class = rsltmgr.extract_from_comparison(comparison, 'similar best class')
 
-    # viz.plot_metric_radar(names, similar_classes_pct, 'Similar classes (%)')
-    # viz.plot_metric_radar(names, similar_best_class, 'Similar best class')
+    viz.plot_metric_radar(names, similar_classes_pct, 'Top 5 Accuracy')
+    viz.plot_metric_radar(names, similar_best_class, 'Top 1 Accuracy')
 
     # viz.plot_compare_metrics(names, similar_classes_pct, similar_best_class)
 
@@ -117,8 +130,10 @@ if __name__ == '__main__':
     # image = Path(path / name)
 
     # Looking for a way to compare size of source and compressed items
-    # print(image.stat().st_size)
+    # print(f"Source image size: {image.stat().st_size}")
 
-    res_folder_nonexist = Path(res_folder / '3')
-    yu = rsltmgr.load_summary_results(res_folder, 'MobileNetV2', 5)
-    print(yu)
+    # yu = rsltmgr.load_summary_results(res_folder, 'MobileNetV2', 5)
+    # print(yu)
+    # print("-" * 80)
+
+    # help(processor)
